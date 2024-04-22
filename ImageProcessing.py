d4b5269b07ebdbf387965c8f6e840e7e4a00c9cc
Brenn Hong <brennhong@gmail.com>
2024-04-23 04:18:27 +0800

    Reworked logic to work with SQLite3.

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,24 +1,28 @@
 import os
 import re
+import json
+import sqlite3
 from typing import Optional
 
 
 class Frame:
 
     # Properties
-    def __init__(self, frame_folder_path):
+    def __init__(self, frame_number: Optional[int], frame_folder_path):
+        self._frame_number = frame_number
         self._frame_folder_path = frame_folder_path
         self._frame_folder = os.path.basename(self._frame_folder_path)
 
-        extracted_frame_number = self.extract_frame_number(self._frame_folder)
-        self._frame_number = extracted_frame_number[0]
+        #extracted_frame_number = self.extract_frame_number(self._frame_folder)
+
+        #self._frame_number = extracted_frame_number[0]
 
         self._frame_image = self.find_frame()
-        self._label_path = self.find_label_path()
-        self._crops_list_path = self.find_crops_list_path()  #crops path
-        self._label_list = self.read_label()
+        self._label_file_path = self.find_label_file_path()
+        #self._crops_list_path = self.find_crops_list_path()  #crops path
+        #self._label_list = self.read_label()
 
-        self._crop_list = self.create_crop_list()
+        #self._crop_list = self.create_crop_list()
 
     @property
     def frame_folder_path(self):
@@ -37,24 +41,28 @@ class Frame:
         return self._frame_image
 
     @property
-    def label_path(self):
-        return self._label_path
+    def label_file_path(self):
+        return self._label_file_path
 
-    @property
-    def crops_list_path(self):
-        return self._crops_list_path
+    # @property
+    # def label_path(self):
+    #     return self._label_file_path
+    #
+    # @property
+    # def crops_list_path(self):
+    #     return self._crops_list_path
 
-    @property
-    def label_list(self):
-        return self._label_list
+    # @property
+    # def label_list(self):
+    #     return self._label_list
 
-    @property
-    def crop_list(self):
-        return self._crop_list
+    # @property
+    # def crop_list(self):
+    #     return self._crop_list
 
-    @crop_list.setter
-    def crop_list(self, crop_list):
-        self._crop_list = crop_list
+    # @crop_list.setter
+    # def crop_list(self, crop_list):
+    #     self._crop_list = crop_list
 
     def extract_frame_number(self, folder_name):
         extract_frame_number = re.findall(r"\d+\.?\d*", self._frame_folder)
@@ -70,7 +78,7 @@ class Frame:
                 break
         return frame_img
 
-    def find_label_path(self):
+    def find_label_file_path(self):
         label_folder_path = os.path.join(self.frame_folder_path, "labels")
         label_file_path = None
         for file in os.listdir(label_folder_path):
@@ -80,7 +88,7 @@ class Frame:
         return label_file_path
 
     def read_label(self):
-        label_file_path = self._label_path
+        label_file_path = self._label_file_path
         try:
             with open(label_file_path, 'r') as file:
                 label_file = file.readlines()
@@ -94,6 +102,7 @@ class Frame:
         except Exception as e:
             print(f"Error reading file '{label_file_path}': {e}")
 
+
     def find_crops_list_path(self):
         crops_folder_path = os.path.join(self._frame_folder_path, "crops", "person")
         crops_path_list = []
@@ -102,21 +111,70 @@ class Frame:
             crops_path_list.append(crops_path)
         return crops_path_list
 
-    def create_crop_list(self):
-
-        crop_list = []
+    def create_person_list(self, indexed_person_list):
+        label_list = self.read_label()
+        new_indexed_person_list = []
         counter = 0
-        while (counter < len(self.label_list)):
-            crop = self.crops_list_path
-            label = self.label_list
-            label_line = self.label_list[counter]
-            label_split = label_line.split(' ')
-            crop_id = label_split[5]
+        found_match = False
 
-            new_crop = Crop(yolo_class=0, crop_path=crop[counter], crop_id=crop_id, label=label_split,
-                            frame_image=self.frame_image, frame_number=self.frame_number)
+        if len(indexed_person_list) == 0 and len(label_list) > counter:
+            label_line = label_list[counter]
+            label_split = label_line.split(' ')
+            person_id = int(label_split[5])
+            new_person = Person(person_id=person_id)
+            new_indexed_person_list.append(new_person)
+            return new_indexed_person_list
+
+        while len(label_list) > counter:
+            label_line = label_list[counter]
+            label_split = label_line.split(' ')
+            person_id = int(label_split[5])
+
+            new_person = Person(person_id=person_id)
+
+            for person in indexed_person_list:
+                if new_person.person_id == person.person_id:
+                    found_match = True
+                    break
+
+            if found_match:
+                counter += 1
+                found_match = False
+                continue
+
+            new_indexed_person_list.append(new_person)
+            counter += 1
+
+
+        return new_indexed_person_list
+
+    # for person in indexed_person_list:
+    #     if new_person.person_id == person.person_id:
+    #         counter += 1
+    #         found_match = True
+    #         continue
+    # if found_match:
+    #     new_indexed_person_list.append(new_person)
+    #     counter += 1
+    #     found_match = False
+
+    def create_crop_list(self, crop_number):
+        label_list = self.read_label()
+        crops_list_path = self.find_crops_list_path()
+        crop_list = []
+        crop_id = crop_number + 1
+        counter = 0
+        while (counter < len(label_list)):
+            crop = crops_list_path
+            label_line = label_list[counter]
+            label_split = label_line.split(' ')
+            person_id = int(label_split[5])
+
+            new_crop = Crop(crop_id=crop_id, person_id=person_id, frame_number=self.frame_number, yolo_class=0, crop_path=crop[counter], label_line=label_line,
+                            frame_image=self.frame_image)
             crop_list.append(new_crop)
             counter += 1
+            crop_id += 1
 
         return crop_list
 
@@ -124,32 +182,45 @@ class Frame:
 class Crop:
 
     #def __init__(self, yolo_class, crop_path, label, frame: Optional[Frame]):
-    def __init__(self, yolo_class, crop_path, crop_id, label, frame_image, frame_number):
+    def __init__(self, crop_id, person_id, frame_number, yolo_class, crop_path, label_line, frame_image):
+        self._crop_id = crop_id
+        self._person_id = person_id
+        self._frame_number = frame_number
         self._yolo_class = yolo_class
         self._crop_path = crop_path
-        self._label = label
+        self._label_line = label_line
         self._frame_image = frame_image
-        self._frame_number = frame_number
-        self._crop_id = crop_id
 
-        self._frame_coordinates = self._set_frame_coordinates()
+
+        # self._frame_coordinates = self._set_frame_coordinates()
         self._hash_value = None
 
-    def _set_frame_coordinates(self):
-        coordinates = []
-        coordinates.append(self._label[1])
-        coordinates.append(self._label[2])
-        coordinates.append(self._label[3])
-        coordinates.append(self._label[4])
-        return coordinates
+    # def _set_frame_coordinates(self):
+    #     coordinates = []
+    #     coordinates.append(self._label[1])
+    #     coordinates.append(self._label[2])
+    #     coordinates.append(self._label[3])
+    #     coordinates.append(self._label[4])
+    #     return coordinates
 
     @property
     def crop_path(self):
         return self._crop_path
 
     @property
-    def label(self):
-        return self._label
+    def person_id(self):
+        return self._person_id
+
+    @property
+    def yolo_class(self):
+        return self._yolo_class
+
+    @property
+    def label_line(self):
+        return self._label_line
+    # @property
+    # def label(self):
+    #     return self._label
 
     @property
     def frame_image(self):
@@ -174,25 +245,25 @@ class Crop:
 
 class Person:
 
-    def __init__(self, person_id):
+    def __init__(self, person_id: Optional[int]):
         self._person_id = person_id
-        self._crop_list = []
+        #self._crop_list = []
         self._has_montage = False
 
-    def add_crop(self, crop):
-        self._crop_list.append(crop)
+    # def add_crop(self, crop):
+    #     self._crop_list.append(crop)
 
     @property
     def person_id(self):
         return self._person_id
 
-    @property
-    def crop_list(self):
-        return self._crop_list
+    # @property
+    # def crop_list(self):
+    #     return self._crop_list
 
-    @crop_list.setter
-    def crop_list(self, crop_list):
-        self._crop_list = crop_list
+    # @crop_list.setter
+    # def crop_list(self, crop_list):
+    #     self._crop_list = crop_list
 
     @property
     def has_montage(self):
--- VideoIndexing.py
+++ VideoIndexing.py
@@ -1,7 +1,13 @@
 import os
 import re
 import shutil
+import sys
+from typing import Optional
+
 import cv2
+import sqlite3
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
+import json
 from ImageProcessing import Frame, Crop, Person
 
 
@@ -9,9 +15,10 @@ class VideoDetails:
 
     def __init__(self, video_title):
         self._video_title = video_title
-        self._person_list = None
-        self._processed_frames_list = None
         self._is_processed = False
+        # self._person_list = None
+        # self._processed_frames_list = None
+
 
         self.input_folder = r".\input"
         self.output_folder = r".\output"
@@ -21,6 +28,118 @@ class VideoDetails:
         self.make_directory(self.input_folder)
         self.make_directory(self.output_folder)
         self.make_directory(self.processed_folder)
+
+    def save_videodetails_sqldata(self):
+        conn = sqlite3.connect('main.db')
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO VideoDetails(video_title, is_processed) VALUES (?, ?)", (self.video_title, self.is_processed))
+        conn.commit()
+        conn.close()
+
+    def save_frame_sqldata(self, processed_frame_list : Optional[list[Frame]]):
+        conn = sqlite3.connect('main.db')
+        cursor = conn.cursor()
+        for frame in processed_frame_list:
+            cursor.execute(f"INSERT INTO Frame(frame_number, frame_folder_path, frame_folder, frame_image, label_file_path) "
+                                f"VALUES (?, ?, ?, ?, ?)",
+                    (frame.frame_number, frame.frame_folder_path, frame.frame_folder, frame.frame_image, frame.label_file_path))
+        conn.commit()
+        conn.close()
+
+    def save_person_sqldata(self, indexed_persons_list : Optional[list[Person]]):
+        #conn = sqlite3.connect(r'..\DBMS_Database\main.db')
+        conn = sqlite3.connect(r'main.db')
+        cursor = conn.cursor()
+        for person in indexed_persons_list:
+            cursor.execute(f"INSERT INTO Person(person_id, has_montage) "
+                                f"VALUES (?, ?)",
+                    (person.person_id, person.has_montage))
+        conn.commit()
+        conn.close()
+    def save_crop_sqldata(self, crop_list : Optional[list[Crop]]):
+        conn = sqlite3.connect('main.db')
+        cursor = conn.cursor()
+        for crop in crop_list:
+            cursor.execute(f"INSERT INTO Crop(crop_id, person_id, frame_number, yolo_class, crop_path, label_line, frame_image)"
+                                f" VALUES (?, ?, ?, ?, ?, ? ,?)",
+                    (crop.crop_id, crop.person_id, crop.frame_number, crop.yolo_class, crop.crop_path, crop.label_line, crop.frame_image))
+        conn.commit()
+        conn.close()
+
+    def load_sql_data(self):
+        conn = sqlite3.connect('main.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM VideoDetails")
+        all_entries = cursor.fetchall()
+        conn.close()
+
+        list = []
+        for detail in all_entries:
+            list.append(VideoDetails(detail, detail.video_detail))
+
+    def index_persons(self):
+        def extract_frame_number(frame_folder):
+            extract_frame_number = re.findall(r"\d+\.?\d*", frame_folder)
+            if extract_frame_number == []:
+                extract_frame_number = "1"
+            return extract_frame_number
+
+        if not os.path.exists(self.inferred_folder):
+            print("ERROR! No inferencing occurred")
+            pass
+
+        indexed_persons_list = []
+        processed_frames_list = []
+        crop_list = []
+        crop_count = 0
+        frame_folders = os.path.join(self.inferred_folder, self.video_title)
+        for folder in os.listdir(frame_folders):
+            frame_folder_path = os.path.join(frame_folders, folder)
+            if (len(os.listdir(frame_folder_path)) != 3):  #Check if the folder is not corrupted
+                continue
+
+            frame_number = extract_frame_number(frame_folder=os.path.basename(frame_folder_path))
+            frame = Frame(frame_number=int(frame_number[0]), frame_folder_path=frame_folder_path)
+
+            person_list = frame.create_person_list(indexed_persons_list)
+            indexed_persons_list.extend(person_list)
+
+            # Create crops
+            new_crop_list = frame.create_crop_list(crop_count)
+            crop_list.extend(new_crop_list)
+            crop_count += len(frame.find_crops_list_path())
+
+            processed_frames_list.append(frame)
+
+        self.is_processed = True
+        self.save_videodetails_sqldata()
+        self.save_frame_sqldata(processed_frame_list=processed_frames_list)
+        self.save_person_sqldata(indexed_persons_list=indexed_persons_list)
+        self.save_crop_sqldata(crop_list=crop_list)
+
+    def build_indexed_persons(self):
+        if self.person_list is None:
+            print("Video is not indexed")
+            pass
+        video_build_directory = os.path.join(self.processed_folder, self.video_title)
+        self.make_directory(video_build_directory)
+
+        for person in self.person_list:
+            person_folder_name = "person_"
+            new_person_folder = os.path.join(video_build_directory, person_folder_name + person.person_id)
+            if not os.path.exists(new_person_folder):
+                os.mkdir(new_person_folder)
+            else:
+                continue
+            for crop in person.crop_list:
+                shutil.copyfile(crop.crop_path, new_person_folder + "/" + crop.frame_number + ".jpg")
+
+
+    def __str__(self):
+        return f"MyClass(data={self.video_title})"
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
 
     @property
     def video_title(self):
@@ -57,65 +176,34 @@ class VideoDetails:
     def start_inferencing(self):
         pass
 
-    def index_persons(self):
-        if not os.path.exists(self.inferred_folder):
-            print("ERROR! No inferencing occurred")
-            pass
-
-        indexed_persons_list = []
-        processed_frames_list = []
-        frame_folders = os.path.join(self.inferred_folder, self.video_title)
-        for folder in os.listdir(frame_folders):
-            folder_path = os.path.join(frame_folders, folder)
-            if (len(os.listdir(folder_path)) != 3):  #Check if the folder is not corrupted
-                continue
-
-            frame = Frame(folder_path)
-            processed_frames_list.append(frame)
-            # Create crops
-            for crop in frame.crop_list:
-
-                new_person = Person(person_id=crop.crop_id)
-                # Create Person
-                if len(indexed_persons_list) == 0:
-                    new_person.crop_list.append(crop)
-                    indexed_persons_list.append(new_person)
-                else:
-                    person_not_found = False
-                    for person in indexed_persons_list:
-                        if person.person_id == new_person.person_id:  # Checking if person exists
-                            person.crop_list.append(crop)
-                            person_not_found = True
-                            break
-                    if person_not_found == False:
-                        new_person.crop_list.append(crop)  # what happened???
-                        indexed_persons_list.append(new_person)
-
-        self.is_processed = True
-        self.processed_frames_list = processed_frames_list
-        self.person_list = indexed_persons_list
-
-    def build_indexed_persons(self):
-        if self.person_list is None:
-            print("Video is not indexed")
-            pass
-        video_build_directory = os.path.join(self.processed_folder, self.video_title)
-        self.make_directory(video_build_directory)
-
-        for person in self.person_list:
-            person_folder_name = "person_"
-            new_person_folder = os.path.join(video_build_directory, person_folder_name + person.person_id)
-            if not os.path.exists(new_person_folder):
-                os.mkdir(new_person_folder)
-            else:
-                continue
-            for crop in person.crop_list:
-                shutil.copyfile(crop.crop_path, new_person_folder + "/" + crop.frame_number + ".jpg")
-
-
-
-
 if __name__ == "__main__":
     x = VideoDetails("Thesis_FullOfficeCut_T300_5mins")
     x.index_persons()
-    x.build_indexed_persons()
+    #x.sql()
+    #x.index_persons()
+    #x.build_indexed_persons()
+
+    def save_class(obj):
+        conn = sqlite3.connect("my_database.db")
+        cursor = conn.cursor()
+
+        serialized_data = obj.to_json()
+        try:
+            cursor.execute("")
+            conn.commit()
+            print("Class saved successfully")
+        except sqlite3.Error as e:
+            print("Error saving class:", e)
+        finally:
+            conn.close()
+
+    def load_class():
+        conn = sqlite3.connect("my_database.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("SELECT class_date FROM classes")
+            #row = cursor.fetchone()
+        finally:
+            pass
+
